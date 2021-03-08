b0d8de739943fa208acdc489a12ac2fcb48e3c40
borngasp <56008554+borngasp@users.noreply.github.com>
2021-03-07 20:35:11 -0800

    Added test 2 module
    To do this test all that needed to be done was to add a nested if that checks if the year is also divisible by 100.

--- LeapYear.py
+++ LeapYear.py
@@ -2,6 +2,9 @@
 
 def LeapYear(x):
     if x%4 == 0:
-        return "Is a leap year"
+        if x%100 == 0:
+            return "Not a leap year"
+        else:
+            return "Is a leap year"
     else:
         return "Not a leap year"
