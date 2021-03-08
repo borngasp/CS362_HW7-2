5ad28490ba303109c04f2eb5a44e6a21b0b70568
borngasp <56008554+borngasp@users.noreply.github.com>
2021-03-07 20:36:42 -0800

    Added test 3
    This last test is for checking that years divisible by 4, 100, and 400 are counted as leap years properly.

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -9,6 +9,9 @@ class FizzBuzzTestCases(unittest.TestCase):
     
     def test2(self):
         self.assertEqual(LeapYear.LeapYear(2100),"Not a leap year")
+
+    def test3(self):
+        self.assertEqual(LeapYear.LeapYear(2400),"Is a leap year")
     
 if __name__ == "__main__":
     unittest.main()
