20c2a149a71bfa42f24bb423583635f05ebedae4
borngasp <56008554+borngasp@users.noreply.github.com>
2021-03-07 20:38:46 -0800

    Added test 3 module
    To do this test another nested loop was added that checks if the year that is divisible by 4 and 100 is also divisible by 400. If it is, it is set to be a leap year.

--- LeapYear.py
+++ LeapYear.py
@@ -3,7 +3,10 @@
 def LeapYear(x):
     if x%4 == 0:
         if x%100 == 0:
-            return "Not a leap year"
+            if x%400 ==0:
+                return "Is a leap year"
+            else: 
+                return "Not a leap year"
         else:
             return "Is a leap year"
     else:
